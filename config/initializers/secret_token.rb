e08b00d5c45749d1fdba0bec740b23e080400be7
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-20 09:50:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuickShark::Application.config.secret_key_base = 'b6bc856ba1cd3fd41612b04e0265634e96b29b1c0a53d28e0b58ac1ef639a7a1e2ca307fd914dd2c5d40d6d99f8c79f6ec1cf59c159fb42d6d9591497cf8d6b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuickShark::Application.config.session_store :cookie_store, key: '_quick_shark_session'
