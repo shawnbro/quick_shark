856d9a2088faf55fb7ce8af46624ace555c360a6
kevinmcalear <kevin.mcalear@gmail.com>
2014-03-20 16:26:26 -0400

    added the guts of the graph path

--- spec/features/user_can_view_saved_journies.rb
+++ spec/features/user_can_view_saved_journies.rb
@@ -11,15 +11,23 @@ describe "a user views a journey" do
 
 
     visit journey_path(journey.id)
+    save_and_open_page
     within ".starting-word" do
       expect(page).to have_content(topic1.name)
     end
+    
     within ".ending-word" do
       expect(page).to have_content(topic3.name)
     end
+    
     within ".tangent-number" do
       expect(page).to have_content("3")
+    end
 
+    within ".journey-graph" do
+      expect(page).to have_content(topic1.name)
+      expect(page).to have_content(topic2.name)
+      expect(page).to have_content(topic3.name)
     end
   end
 end
