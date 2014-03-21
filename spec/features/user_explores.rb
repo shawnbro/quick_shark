12e21087948de6256cb6d61efe3cc3c0c1a6c794
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-21 16:30:06 -0400

    search button

--- spec/features/user_explores.rb
+++ spec/features/user_explores.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe 'a user explores' do 
   it "clicks the explore button which randomly generates a word/graph" do 
     visit root_path
-    save_and_open_page
     click_button "Explore"
-    expect(page).to have_content('div#viz')
+    page.should have_css('div#viz')
   end
 end
