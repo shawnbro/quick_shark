c3da7f30b305239cec0028d31df1c56e5b456b4e
Rory Budnick <rorybudnick@gmail.com>
2014-03-20 20:51:20 -0400

    user can view their account page

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,6 +1,12 @@
 class JourneysController < ApplicationController
+
   def show
     @journey = Journey.find(params[:id])
+    if session[:user_id].nil?
+      render nothing: true, status: 401
+    elsif session[:user_id] = current_user.id
+      render(:show)      
+    end
   end
 
 end
