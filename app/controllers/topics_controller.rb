6eef0cd49a9e86b1567a26aa8a7cefea2c9ee0a6
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-23 15:06:27 -0400

    fixed bug

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -17,7 +17,7 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.find_by(id: params[:id])
     @journey = Journey.find_by(id: @topic.journey_id)  
-    @word_association = get_word_associations(@topic.name.downcase!)
+    @word_association = get_word_associations(@topic.name)
     @videos = get_youtube_vids(@topic.name).take(4)
   end
 
