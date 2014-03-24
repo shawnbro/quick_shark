d730b98db3bd1e693740ab0b8c8ccd5eaec8834a
Quincy Iheme <qiheme@gmail.com>
2014-03-24 17:18:36 -0400

    fixed refresh button

--- app/assets/javascripts/movingdivs.js
+++ app/assets/javascripts/movingdivs.js
@@ -1,5 +1,6 @@
-
+$(document).ready(function(){
   KeyboardJS.on('down', function() {
+    console.log('down firing');
     reset();
     $("div#define")[0].style.top = "5%";
     $("div#pictures")[0].style.top = "105%";
@@ -10,9 +11,10 @@
       dataType: "text",
       success: function(result){$("div#define p").text(result)}
     });
-  });
+  }); // on 'down'
 
   KeyboardJS.on('up', function() {
+    console.log('up firing');
     reset();
     $("div#pictures")[0].style.top = "5%";
     $("div#define")[0].style.top = "-105%";
@@ -30,9 +32,10 @@
         }
       }
     });
-  });
+  }); // on 'up'
 
   KeyboardJS.on('left', function() {
+    console.log('left firing');
     reset();
     $("div#stats")[0].style.left = "5%";
     $("div#videos")[0].style.left = "-105%";
@@ -48,9 +51,12 @@
         }
       }
     })
-  });
+  }); // on 'left'
 
   KeyboardJS.on('right', function() {
+    console.log('right firing');
+    var videos;
+
     reset();
     $("div#videos")[0].style.left = "5%";
     $("div#stats")[0].style.left = "105%";
@@ -59,37 +65,40 @@
       data: {name: $("h1").text()},
       dataType: "JSON",
       success: function(result){
+        newVideos = result;
+
         $('iframe').remove();
+        $('div#video').remove();
         $('button').remove();
         $("div#videos").append('<div class="videoWrap">');
         for ( var i = 0; i < 4; i++ ){
-        $('<div id=video><iframe width="420" height="200"></div>')
-          .attr( "src", 'http://www.youtube.com/embed/' + result.items[i].id.videoId + '')
-          .appendTo('div.videoWrap')
-          .css('border-radius', '10px');
+          $('<div id=video><iframe width="400" height="200" src="http://www.youtube.com/embed/' + result.items[i].id.videoId + '"></div>').appendTo('div.videoWrap').css('border-radius', '10px');
         }
-      $("div#videos").append($("<button>").text("Refresh"));
-      $('button').css('border-radius', '10px');
-      $('div.videoWrap').css('display', 'block')
-        .css('width', '1000px')
-        .css('margin-left', 'auto')
-        .css('margin-right', 'auto');
-      $('div#video').css('display', 'inline-block');
+        $("div#videos").append($("<button id='refresh'></button>").text("Refresh"));
+        $('button').css('border-radius', '10px');
+        $('div.videoWrap').css('display', 'block').css('width', '1000px').css('margin-left', 'auto').css('margin-right', 'auto');
+        $('div#video').css('display', 'inline-block');
       }
-    })
-  });
+    }); // end ajax
+  }); // on 'right'
 
-  // var videoIndex = 4;
+  var videoIndex = 4;
 
-  // var refreshButton = function(){
-  //   $('button.refresh').click(function() {
-  //   $('iframe').remove();
-  //   for ( var i = 0; i < 4; i++ ){
-  //       $('<iframe width="420" height="345">').attr( "src", 'http://www.youtube.com/embed/' + videos.items[(i+videoIndex) % 50].id.videoId + '').appendTo('div.Youtube-videos');
-  //     }
-  //   videoIndex += 4;
-  //   });
-  // }
+  $('#videos').on("click", "button", function() {
+    $('iframe').remove();
+    $('div#video').remove();
+    $('button').remove();
+    for ( var i = 0; i < 4; i++ ){
+      $('<div id=video><iframe width="400" height="200" src="http://www.youtube.com/embed/' + newVideos.items[(i+videoIndex) % 50].id.videoId + '"></div>').appendTo('div.videoWrap').css('border-radius', '10px');
+    }
+    $("div#videos").append($("<button>").text("Refresh"));
+    $('button').css('border-radius', '10px');
+    $('div.videoWrap').css('display', 'block').css('width', '1000px').css('margin-left', 'auto').css('margin-right', 'auto');
+    $('div#video').css('display', 'inline-block');
+
+    videoIndex += 4;
+
+  }); // click button#refresh
 
   var reset = function() {
     $("div#define")[0].style.top = "-105%";
@@ -99,3 +108,5 @@
   }
 
   KeyboardJS.on('c', reset);
+
+}); // end ready
