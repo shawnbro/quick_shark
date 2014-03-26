21d2c9aa9b5137447c05927828ddc8303cb84398
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-25 23:15:21 -0400

    i like it better when the instructions just stay put

--- spec/features/user_sees_instructions.rb
+++ spec/features/user_sees_instructions.rb
@@ -6,6 +6,8 @@ describe 'user sees nav options around perimeter when looking at a topic', :js =
     visit root_path
     fill_in 'topic[topic]', with: "clarity"
     find("#exploreB").click
-
+    within("div#side-instructions") do 
+      expect( page ).to have_content("Click on a word...Push the arrow keys...Click and drag...Scroll to zoom.")
+    end
   end
 end
